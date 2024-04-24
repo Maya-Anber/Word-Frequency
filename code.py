67efbc5370998de1c2489b6b6d58651b00931ec6
Maya <143032303+Maya-Anber@users.noreply.github.com>
2024-04-24 22:54:37 +0200

    code.py

--- code.py
+++ code.py
@@ -17,3 +17,10 @@ word_freq = Counter(filtered_words_without_punct)
 for word, freq in word_freq.items():
     print(f"{word}: {freq}")
 
+#to make a new text file without the stop words or punctuation 
+#filtered_text = " ".join(filtered_words_without_punct)
+
+#with open("filtered_random_paragraphs.txt", "w") as outfile:
+    outfile.write(filtered_text)
+    
+
